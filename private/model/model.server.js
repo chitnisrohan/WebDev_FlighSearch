6950124869d4f5760c803577fc075fc6dc9d9093
chitnisrohan <chitnis.r@ccs.neu.edu>
2017-03-29 06:42:26 -0400

    changed env variable names

--- private/model/model.server.js
+++ private/model/model.server.js
@@ -3,11 +3,12 @@ module.exports = function () {
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
     if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
+        connectionString = process.env.MLAB_UNAME + ":" +
+            process.env.MLAB_PWD + "@" +
+            process.env.MLAB_HOST2 + ':' +
+            process.env.MLAB_PORT2 + '/' +
+            process.env.MLAB_APP_NAME_2;
+        //mongodb://admin:admin@ds143980.mlab.com:43980/heroku_bjfvlp12
     }
 
     mongoose.connect(connectionString);
