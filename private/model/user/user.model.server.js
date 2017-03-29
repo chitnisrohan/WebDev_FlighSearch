58f4c0a3a9bbc55a9d9566e00515d1cd5e86dd10
chitnisrohan <chitnis.r@ccs.neu.edu>
2017-03-29 06:54:53 -0400

    changed usermodel name

--- private/model/user/user.model.server.js
+++ private/model/user/user.model.server.js
@@ -2,7 +2,7 @@ module.exports = function () {
     var model = {};
     var mongoose = require("mongoose");
     var UserSchema = require("./user.schema.server.js")();
-    var UserModel = mongoose.model("UserModel", UserSchema);
+    var UserModel = mongoose.model("NewUserModel", UserSchema);
 
     var Q = require("q");
 
