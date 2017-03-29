4cecd461f74c1fd001e62abebbc163a14d3bb293
chitnisrohan <chitnis.r@ccs.neu.edu>
2017-03-29 00:26:12 -0400

    added support for autocomplete for city

--- public/views/flights/controllers/search.controller.client.js
+++ public/views/flights/controllers/search.controller.client.js
@@ -17,6 +17,54 @@
             } else {
                 vm.isLoggedIn = false;
             }
+
+
+            // Get the <datalist> and <input> elements.
+            var dataList = document.getElementById('cityFrom');
+            var dataList2 = document.getElementById('cityTo');
+            var input = document.getElementById('from');
+            var input2 = document.getElementById('to');
+
+            // Create a new XMLHttpRequest.
+            var request = new XMLHttpRequest();
+
+            // Handle state changes for the request.
+            request.onreadystatechange = function(response) {
+                if (request.readyState === 4) {
+                    if (request.status === 200) {
+                        // Parse the JSON
+                        var jsonOptions = JSON.parse(request.responseText);
+
+                        vm.allCities = jsonOptions.response;
+                        for (var cityInfo in vm.allCities) {
+                            var option = document.createElement('option');
+                            option.value = vm.allCities[cityInfo].name;
+                            var option2 = document.createElement('option');
+                            option2.value = vm.allCities[cityInfo].name;
+//                            dataList.appendChild(option);
+//                            dataList2.appendChild(option2);
+                        }
+
+                        // Update the placeholder text.
+                        input.placeholder = "Boston";
+                        input2.placeholder = "Mumbai";
+                    } else {
+                        // An error occured :(
+                        input.placeholder = "Couldn't load cities";
+                        input2.placeholder = "Couldn't load cities";
+                    }
+                }
+            };
+
+            // Update the placeholder text.
+            input.placeholder = "Loading options...";
+            input2.placeholder = "Loading options...";
+
+            // Set up and make the request.
+            request.open('GET', 'https://crossorigin.me/https://iatacodes.org/api/v6/cities?api_key=7cf86b00-61f2-47df-a5d0-d56b5bf819bb', true);
+//    request.open('GET', 'http://cors.io/?u=http://iatacodes.org/api/v6/autocomplete?api_key=7cf86b00-61f2-47df-a5d0-d56b5bf819bb&query=boston', true);
+            request.send();
+
         }
         init();
 
@@ -43,6 +91,14 @@
 
         function searchFlight(journey) {
             var searchUrl;
+            for (var cityInfo in vm.allCities) {
+                if (vm.allCities[cityInfo].name == journey.source) {
+                    journey.source = vm.allCities[cityInfo].code;
+                }
+                if (vm.allCities[cityInfo].name == journey.destination) {
+                    journey.destination = vm.allCities[cityInfo].code;
+                }
+            }
             if (typeof journey.returnDate != "undefined") {
                 searchUrl = "/flight/search/SRC/"+journey.source+"/DEST/"+journey.destination
                     +"/DEPART/"+journey.departDate.toISOString().substring(0, 10)
