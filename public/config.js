d5adaf7c23228c8e86ccc9b8d161a121e1ba57ec
chitnisrohan <chitnis.r@ccs.neu.edu>
2017-04-08 06:10:54 -0400

    removed unnecessory comma

--- public/config.js
+++ public/config.js
@@ -57,7 +57,7 @@
             .when("/flight/search/SRC/:src/DEST/:dest/DEPART/:dept/RETURN/:ret/ADULTS/:adults/CHILD/:child/CLASS/:class", {
                 templateUrl: "views/flights/templates/search.results.view.client.html",
                 controller: "SearchResultController",
-                controllerAs: "model",
+                controllerAs: "model"
             })
             .when("/login", {
                 templateUrl: "views/user/templates/login.view.client.html",
