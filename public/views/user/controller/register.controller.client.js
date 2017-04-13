1a82dc83583ac199851cecd34eeadb3d4dbdfb22
chitnisrohan <chitnis.r@ccs.neu.edu>
2017-04-13 01:33:25 -0400

    merged forgot password list

--- public/views/user/controller/register.controller.client.js
+++ public/views/user/controller/register.controller.client.js
@@ -11,10 +11,17 @@
 
         function init() {
             vm.userType = userType;
+            vm.questionOptions = {availableQuestions : [{id : '1',name :'What is your birth city?'},
+                {id : '2',name :'What is your mother\'s maiden name?'},
+                {id : '3',name :'What is your favorite sport?'},
+                {id : '4',name :'What is your favorite color?'},
+                {id : '5',name :'What is your favorite car?'}],
+                passwordRecoveryQuestion : {id : '1',name :'What is your birth city?'}} ;
         }
         init();
 
         function registerUser(newUser) {
+            newUser.passwordRecoveryQuestion = vm.questionOptions.passwordRecoveryQuestion.name;
             newUser.userType = vm.userType;
             if (newUser.password === newUser.verify.password) {
                 UserService
